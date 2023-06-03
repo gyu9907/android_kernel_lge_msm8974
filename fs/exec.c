3c30bb4fe4e8c0ca370d427a4446b90772cc08b2
jb881122 <joeybeattie5@gmail.com>
2023-06-03 02:47:48 +0000

    Revert "kernel: Only expose su when daemon is running"
    This reverts commit 53f55e99bc2944802094a18426a9dfd574a161aa.

--- fs/exec.c
+++ fs/exec.c
@@ -1640,17 +1640,9 @@ static int do_execve_common(const char *filename,
 	if (retval < 0)
 		goto out;
 
-	/* search_binary_handler can release file and it may be freed */
-	is_su = d_is_su(file->f_dentry);
-
 	retval = search_binary_handler(bprm,regs);
 	if (retval < 0)
 		goto out;
-
-	if (is_su && capable(CAP_SYS_ADMIN)) {
-		current->flags |= PF_SU;
-		su_exec();
-	}
 
 	/* execve succeeded */
 	current->fs->in_exec = 0;
